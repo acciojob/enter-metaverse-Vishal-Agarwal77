edced8259a68d6e190a7cd11b7eb27ca2fd8b7ae
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 17:45:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-//your JS code here. If required.
-const p=document.getElementById("status");
-const btn=document.getElementById("enterBtn");
-const body=document.getElementsByTagName("body")[0];
-let count=0;
+let btn=document.getElementById("enterBtn");
 btn.addEventListener("click",()=>{
-	if(count>0){
-		return ;
-	}
-	const h1=document.createElement("h1");
-	h1.id="status";
-	h1.innerHTML="Entered Metaverse";
-	p.innerHTML="";
-	p.appendChild(h1);
-	body.insertBefore(h1,btn);
-	count++;
-});
-
+	document.getElementById("status").innerHTML="<h1>Entered Metaverse</h1>";	
+})
