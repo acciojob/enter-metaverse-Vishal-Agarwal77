42b76db6d12df05ec67d7b98a70981c9eab8f08c
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 16:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ btn.addEventListener("click",()=>{
 	if(count>0){
 		return ;
 	}
-	p.remove();
 	const h1=document.createElement("h1");
 	h1.id="status";
 	h1.innerHTML="Entered Metaverse";
+	p.innerHTML="";
+	p.appendChild(h1);
 	body.insertBefore(h1,btn);
 	count++;
 });
