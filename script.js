8025d6af6f09470026fc7465142534b9b4f68a35
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 14:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const p=document.getElementById("status");
+const btn=document.getElementById("enterBtn");
+const body=document.getElementsByTagName("body")[0];
+let count=0;
+btn.addEventListener("click",()=>{
+	if(count>0){
+		return ;
+	}
+	p.remove();
+	const h1=document.createElement("h1");
+	h1.innerHTML="Entered Metaverse";
+	body.insertBefore(h1,btn);
+	count++;
+});
+
